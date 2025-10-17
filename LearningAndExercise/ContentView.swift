04af3838cf4fa79202b3cbb6c0fff6da9d93b0d1
Pradyumna Rout <pradyumnarout1433@gmail.com>
2025-10-17 11:28:57 +0530

    Initial Commit

---
+++ LearningAndExercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearningAndExercise
+//
+//  Created by hb on 17/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearningAndExercise/LearningAndExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAndExerciseApp.swift
+//  LearningAndExercise
+//
+//  Created by hb on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LearningAndExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
